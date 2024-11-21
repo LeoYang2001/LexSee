f132fdefc9b7c1836c976c51bd6facd78146a1fe
pentabi <tabitosatoh@gmail.com>
2024-11-20 21:35:53 -0500

    change version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "LexSee",
     slug: "lexsee",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/bookmark.png",
     userInterfaceStyle: "light",
