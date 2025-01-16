d786a78eae92f8f5e54726a69f71403d0e6fad94
LeoYang2001 <jya261@uky.edu>
2025-01-16 11:10:28 -0500

    wordFlexCardCompleted

--- components-shared/WordFlexCard.js
+++ components-shared/WordFlexCard.js
@@ -1,21 +1,132 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, Image, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ArrowRight, Volume2 } from "lucide-react-native";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { BlurView } from "expo-blur";
+import { useNavigation } from "@react-navigation/native";
 
-const WordFlexCard = () => {
+const WordFlexCard = ({ wordItem, ifActive, setActiveCardId, navigation }) => {
+  if (!wordItem) return null;
+
+  // Reanimated value to control the height of the card
+  const cardHeight = useSharedValue(132);
+
+  // Update card height based on the `ifActive` prop
+  useEffect(() => {
+    cardHeight.value = ifActive ? 207 : 132;
+  }, [ifActive]);
+
+  // Animated style for the height transition
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      height: withSpring(cardHeight.value, { damping: 20, stiffness: 200 }),
+    };
+  });
+
   return (
-    <View
+    <Pressable
       style={{
-        height: 132,
+        width: "100%",
         borderRadius: 12,
       }}
-      className="w-full flex flex-col  border p-4"
+      className="w-full flex flex-col relative mb-4"
+      onPress={() => {
+        if (!ifActive) {
+          setActiveCardId(wordItem.id);
+        } else {
+          setActiveCardId(null);
+        }
+      }}
     >
-      <View className="w-full border">
-        <Text className="font-semibold" style={{ fontSize: 24 }}>
-          Explore
-        </Text>
-      </View>
-    </View>
+      <Animated.View style={[{ width: "100%" }, animatedStyle]}>
+        <Image
+          className="absolute"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: 12,
+          }}
+          source={{
+            uri: wordItem.imgUrl,
+          }}
+          resizeMode="cover"
+        />
+        <View
+          style={{
+            backgroundColor: "rgba(0,0,0,0.4)",
+            borderRadius: 12,
+          }}
+          className="w-full h-full"
+        ></View>
+        <View className="w-full h-full p-4 absolute z-20 flex flex-col">
+          <View className="flex flex-row justify-between">
+            <Text className="font-semibold text-white" style={{ fontSize: 24 }}>
+              {wordItem.id}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                console.log(navigation);
+                navigation.navigate("Definition");
+              }}
+              className="p-1"
+            >
+              <ArrowRight color={"#fff"} />
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row">
+            <TouchableOpacity className="py-1 flex flex-row">
+              <Text
+                style={{
+                  color: "#FFFFFFB3",
+                }}
+              >
+                {wordItem.phonetics.text}
+              </Text>
+              <Volume2 className="ml-2" color={"#FFFFFFB3"} size={16} />
+            </TouchableOpacity>
+          </View>
+
+          {ifActive && (
+            <View className="w-full flex flex-row mt-auto">
+              <View
+                style={{
+                  height: 20,
+                  backgroundColor: "#222833",
+                  borderRadius: 2,
+                }}
+                className="flex px-1 justify-center border items-center mr-2"
+              >
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: "#fff",
+                  }}
+                >
+                  {wordItem?.meanings[0]?.partOfSpeech}
+                </Text>
+              </View>
+            </View>
+          )}
+          {ifActive && (
+            <BlurView className="w-full flex-1 py-2 px-3 mt-2" intensity={40}>
+              <Text
+                style={{
+                  color: "#fff",
+                  opacity: 0.7,
+                  borderRadius: 10,
+                }}
+              >
+                {wordItem.meanings[0].definitions}
+              </Text>
+            </BlurView>
+          )}
+        </View>
+      </Animated.View>
+    </Pressable>
   );
 };
 
---
+++ screens/definition/nestedSheetPlaceHolder/ExpalnationPage.js
@@ -0,0 +1,156 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { Bookmark, Volume2 } from "lucide-react-native";
+
+const ExpalnationPage = () => {
+  const mockWordItem = {
+    id: "culpable",
+    imgUrl: "https://cdn.langeek.co/photo/23505/original/?type=jpeg",
+    meanings: [
+      {
+        antonyms: [],
+        definitions: [
+          "Actuated by avarice; extremely greedy for wealth or material gain; immoderately desirous of accumulating property.",
+        ],
+        partOfSpeech: "adjective",
+        synonyms: [],
+      },
+      {
+        antonyms: [],
+        definitions: [
+          "Actuated by avarice; extremely greedy for wealth or material gain; immoderately desirous of accumulating property.",
+        ],
+        partOfSpeech: "adjective",
+        synonyms: [],
+      },
+    ],
+    phonetics: {
+      audio:
+        "https://api.dictionaryapi.dev/media/pronunciations/en/culpable-us.mp3",
+      license: {
+        name: "BY-SA 3.0",
+        url: "https://creativecommons.org/licenses/by-sa/3.0",
+      },
+      sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=789670",
+      text: "/ˈkʌlpəbəl/",
+    },
+    timeStamp: "2024-11-03T23:14:17.405Z",
+  };
+  return (
+    <View className="w-full h-full py-8 px-5 flex  flex-col gap-4 ">
+      <View className="w-full flex flex-row justify-between  items-center">
+        <View>
+          <Text
+            className="font-semibold"
+            style={{ fontSize: 28, color: "#fff" }}
+          >
+            {mockWordItem?.id}
+          </Text>
+          <TouchableOpacity className="py-2 flex flex-row items-center">
+            <Text
+              style={{
+                color: "#FFFFFFB3",
+                fontSize: 18,
+              }}
+            >
+              {mockWordItem?.phonetics.text}
+            </Text>
+            <Volume2 className="ml-2" color={"#FFFFFFB3"} size={18} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 48,
+            height: 48,
+            borderRadius: 12,
+            backgroundColor: "#3f3339",
+          }}
+          className="flex justify-center items-center"
+        >
+          <Bookmark color={"#d1461e"} fill={"#d1461e"} />
+        </TouchableOpacity>
+      </View>
+      <View className=" flex flex-row ">
+        {mockWordItem.meanings.map((meaning, index) => (
+          <TouchableOpacity
+            style={{
+              height: 20,
+              backgroundColor: "#ffffff1E",
+              borderRadius: 2,
+            }}
+            className="px-1 mr-2 flex justify-center items-center"
+            key={index}
+          >
+            <Text
+              style={{
+                fontSize: 12,
+                color: "#fff",
+                opacity: 0.7,
+              }}
+            >
+              {meaning.partOfSpeech}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View
+        className="w-full"
+        style={{
+          height: 142,
+        }}
+      >
+        <Image
+          className="absolute"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: 12,
+          }}
+          source={{
+            uri: mockWordItem.imgUrl,
+          }}
+          resizeMode="cover"
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: "#ffffff0a",
+          borderRadius: 12,
+        }}
+        className="p-4 w-full"
+      >
+        {mockWordItem.meanings.map((meaning, index) => (
+          <View
+            style={
+              mockWordItem.meanings.length > 1 &&
+              index !== mockWordItem.meanings.length - 1 && { marginBottom: 16 }
+            }
+            className=" w-full"
+            key={index}
+          >
+            <Text
+              className="font-bold"
+              style={{
+                color: "#fff",
+                opacity: 0.7,
+              }}
+            >
+              {meaning.partOfSpeech}
+            </Text>
+            <Text
+              style={{
+                color: "#fff",
+                opacity: 0.7,
+              }}
+              className="mt-2"
+            >
+              {meaning.definitions}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default ExpalnationPage;
