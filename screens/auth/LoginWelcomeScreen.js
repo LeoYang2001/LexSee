f73d894ef5a3370d502423c82c51b0c3e5f9d888
LeoYang2001 <jya261@uky.edu>
2025-02-28 20:51:47 -0500

    welcomepage update

--- screens/auth/LoginWelcomeScreen.js
+++ screens/auth/LoginWelcomeScreen.js
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
 
@@ -29,43 +35,62 @@ const LoginWelcomeScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <View className="flex-1 bg-black px-6 flex-col justify-between">
-      <View></View>
-      <View>
-        <View>
-          <Text className="text-white font-bold text-4xl">YOUR</Text>
-          <Animated.View
-            style={{
-              left: offSetLeft,
-            }}
-          >
-            <Text
-              style={styles.outlinedText}
-              className="text-4xl font-bold text-black"
-            >
-              LexSee
-            </Text>
-          </Animated.View>
-          <Text className="text-white font-bold text-4xl">VOCABULARY</Text>
+    <View className="flex-1 bg-black flex-col justify-between">
+      {/* background */}
+      <ImageBackground
+        style={{ height: "100%" }}
+        source={require("../../assets/welcomeBg.png")}
+        resizeMode="cover"
+        className="flex flex-col justify-between"
+      >
+        <View className="opacity-0">
+          <Text>demo</Text>
         </View>
-        <View>
+        <View className="translate-y-48 mx-3">
           <Text
-            style={{ fontFamily: "Helvetica Neue" }}
-            className="text-white font-semithin mt-10"
+            style={{
+              fontSize: 32,
+            }}
+            className="text-white  font-semibold"
           >
-            {displayedText}
+            Smart AI Talks
+          </Text>
+          <Text
+            style={{
+              fontSize: 32,
+            }}
+            className="text-white my-6 ml-10 font-semibold"
+          >
+            Captivating Stories.
+          </Text>
+          <Text
+            style={{
+              fontSize: 32,
+            }}
+            className="text-white  font-semibold"
+          >
+            Words That Stick.
           </Text>
         </View>
-      </View>
-
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("SignIn");
-        }}
-        className="bg-white rounded-lg flex justify-center items-center py-3 mb-14 "
-      >
-        <Text className="text-black font-semibold text-lg">Start</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("SignIn");
+          }}
+          style={{
+            backgroundColor: "#FA541C",
+          }}
+          className="  mb-20 rounded-lg flex justify-center items-center py-3 mx-3"
+        >
+          <Text
+            style={{
+              fontSize: 15,
+            }}
+            className="text-white font-semibold "
+          >
+            Start the journey
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
